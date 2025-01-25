cae691cfbc512597ba37e26eadaefae208daddc4
Youngseok <astonisher88@gmail.com>
2025-01-25 18:24:27 -0500

    added util

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+import { calculateSomething } from "./util/sampleUtil";
 
 export const currentTime = (req: Request, res: Response): void => {
     const now = new Date().toISOString();
-    res.status(200).json({ currentTime: now, message: "Hello YSLE" });
+    const numberFromUtil = calculateSomething(1, 2);
+    res.status(200).json({ currentTime: now, message: "Hello YSLE", fromUtil: numberFromUtil });
 };
