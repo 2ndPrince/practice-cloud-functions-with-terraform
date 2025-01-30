1fcd2c8b1633bed9956822db024d140179645f73
Youngseok <astonisher88@gmail.com>
2025-01-30 13:41:48 -0500

    modulaized terraform iam

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,17 +1,17 @@
 resource "google_project_iam_member" "github_actions_sa_user" {
   project = var.project_id
   role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.backend_service.email}"
+  member  = "serviceAccount:${var.backend_service_account_email}"
 }
 
 resource "google_project_iam_member" "github_actions_sa_admin" {
   project = var.project_id
   role    = "roles/iam.serviceAccountAdmin"
-  member  = "serviceAccount:${google_service_account.backend_service.email}"
+  member  = "serviceAccount:${var.backend_service_account_email}"
 }
 
 resource "google_project_iam_member" "github_actions_firestore_admin" {
   project = var.project_id
   role    = "roles/datastore.owner"
-  member  = "serviceAccount:${google_service_account.backend_service.email}"
+  member  = "serviceAccount:${var.backend_service_account_email}"
 }
